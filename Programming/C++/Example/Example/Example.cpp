b9f3c81c8c8ad0f13491f5b88ea2354accb102e9
lkstudio.dante <lkstudio.dante@gmail.com>
2025-07-13 12:41:37 +0900

    no message

--- Programming/C++/Example/Example/Example.cpp
+++ Programming/C++/Example/Example/Example.cpp
@@ -1,15 +1,17 @@
 #define EXAMPLE
 #define TRAINING
 
-#include "03412922000101-KoreaIT_Weekday_Cpp_0001/Example/Example_01/E01Example_01.h"
-#include "03412922000101-KoreaIT_Weekday_Cpp_0001/Example/Example_02/E01Example_02.h"
+#include "Classes/Example/Example_01/E01Example_01.h"
+#include "Classes/Example/Example_02/E01Example_02.h"
 
-#include "03412922000101-KoreaIT_Weekday_Cpp_0001/Training/Training_01/T01Training_01.h"
-#include "03412922000101-KoreaIT_Weekday_Cpp_0001/Training/Training_02/T01Training_02.h"
+#include "Classes/Training/Training_01/T01Training_01.h"
+#include "Classes/Training/Training_02/T01Training_02.h"
 
 /** 메인 함수 */
 int main(int argc, char* args[])
 {
+	srand((unsigned int)time(nullptr));
+
 #if defined EXAMPLE
 	E01Example::E01Example_01(argc, args);
 	//E01Example::E01Example_02(argc, args);
