afdecef9362c78dc756e1937cfe18369811bedb0
lkstudio.dante <lkstudio.dante@gmail.com>
2025-07-30 15:00:35 +0900

    no message

--- Programming/C++/Example/Example/Example.cpp
+++ Programming/C++/Example/Example/Example.cpp
@@ -1,11 +1,13 @@
 #define EXAMPLE
 #define TRAINING
 
+#if defined EXAMPLE
 #include "Classes/Example/Example_01/E01Example_01.h"
 #include "Classes/Example/Example_02/E01Example_02.h"
-
+#elif defined TRAINING
 #include "Classes/Training/Training_01/T01Training_01.h"
 #include "Classes/Training/Training_02/T01Training_02.h"
+#endif // #if defined EXAMPLE
 
 /** 메인 함수 */
 int main(int argc, char* args[])
